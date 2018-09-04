a4404169b05cbd01fef8182684ca44dd54fbf224
ramazan <ramazan.gurpinar.1997@hotmail.com>
2018-09-04 15:01:21 +0300

    three commit

--- js/cp5.js
+++ js/cp5.js
@@ -1,11 +1,5 @@
 
-
-
-
-
 function renkDegistir(renk) {
   var degisken = document.getElementById('chngclr2');
   degisken.style.color = renk;
 }
-//document.getElementById('stil').style.color='renk';
-//
